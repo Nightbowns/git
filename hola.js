5a22ff406620d010b27d4daec8e9ffa169fc56f4
Your Name <victor.patricio.c@gmail.com>
2021-03-10 16:55:05 -0300

    prueba

---
+++ hola.js
@@ -0,0 +1,12 @@
+function saludo(params) {
+    const saludo = params;
+    return saludo;   
+}
+function print(params) {
+    console.log(params); 
+}
+const hola = saludo('Hola Victor');
+const saludarRoro = saludo('heyb0ss');
+const saludar=saludo('hola');
+print(hola,print(saludarRoro),print(saludar));
+// print(saludarRoro);
